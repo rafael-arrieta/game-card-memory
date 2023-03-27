1f7418fb74bdef9dde3af5f3ba98e552ae43f94f
rafael-arrieta <102436201+rafael-arrieta@users.noreply.github.com>
2023-03-26 23:25:53 -0300

    debbuging

--- JS/main.js
+++ JS/main.js
@@ -10,8 +10,8 @@ let lastScore = document.getElementById("lastScore");
 let totalScore = document.getElementById("totalScore");
 let errorInput = document.getElementById("errorInput");
 
-let firstCardSelected = [-1,-1];
-let secondCardSelected = [-1,-1];
+let firstCardSelected = [-1, -1];
+let secondCardSelected = [-1, -1];
 let foundCards = [];
 let winnerFlag = 0;
 let loserFlag = 10;
@@ -19,31 +19,35 @@ let userName;
 let userCurrentScore = 0;
 let userLastScore = 0;
 let userTotalScore = 0;
+let enableReset = true;
 
-function showCardSelected(element,value){
+function showCardSelected(element, value) {
+    element.innerHTML = `
+        <img class="img__card" src="./assets/img${value}.svg" alt="${value}">
+    `;
     element.classList.add("card");
     element.classList.remove("card__reverse");
-    element.innerHTML=`
-    <img class="visible__none img__card" src="./assets/img${value}.svg" alt="${value}">
-    `;
-    element.childNodes[1].classList.add("to__visible");
-    element.childNodes[1].classList.remove("to__invisible");
+    console.log(element.childNodes[1]);
+    setTimeout(() => {
+        element.childNodes[1].classList.add("to__visible");
+    }, 10);
+    
 }
 
-function hideCards(){
+function hideCards() {
     firstCardSelected[2].classList.add("card");
     firstCardSelected[2].classList.add("card__reverse");
-    firstCardSelected[2].childNodes[1].classList.add("to__invisible");
-    firstCardSelected[2].childNodes[1].classList.remove("to__visible");
     
+    firstCardSelected[2].childNodes[1].classList.remove("to__visible");
+
     secondCardSelected[2].classList.add("card");
     secondCardSelected[2].classList.add("card__reverse");
-    secondCardSelected[2].childNodes[1].classList.add("to__invisible");
+    
     secondCardSelected[2].childNodes[1].classList.remove("to__visible");
 
     setTimeout(() => {
-        firstCardSelected[2].innerHTML=``;
-        secondCardSelected[2].innerHTML=``;
+        firstCardSelected[2].innerHTML = ``;
+        secondCardSelected[2].innerHTML = ``;
     }, 500);
 }
 
@@ -51,13 +55,13 @@ function countUserWin() {
     winnerFlag += 1;
     winnerFlagDom.innerText = `${winnerFlag}`;
     userCurrentScore += 200;
-    currentScore.innerText = `${userCurrentScore}`
-    if(winnerFlag==9){
-        userLastScore = userCurrentScore
-        userTotalScore += userLastScore
-        lastScore.innerText = `${userLastScore}`
-        totalScore.innerText = `${userTotalScore}`
-        cardsMainFunc()
+    currentScore.innerText = `${userCurrentScore}`;
+    if (winnerFlag == 9) {
+        userLastScore = userCurrentScore;
+        userTotalScore += userLastScore;
+        lastScore.innerText = `${userLastScore}`;
+        totalScore.innerText = `${userTotalScore}`;
+        cardsMainFunc();
     }
 }
 
@@ -69,43 +73,53 @@ function countUserLifes() {
         cardsMainFunc();
     }
 }
-function clearCards(){
+
+function clearCards() {
     setTimeout(() => {
-    firstCardSelected = [-1,-1,''];
-    secondCardSelected = [-1,-1,''];
-    },1000)
+        firstCardSelected = [-1, -1, ""];
+        secondCardSelected = [-1, -1, ""];
+        enableReset = true;
+        buttonGetNumber.classList.remove("disabled");
+    }, 1000);
 }
 
 function evalCardsValues() {
-    setTimeout(()=>{
-        if(firstCardSelected[0]===secondCardSelected[0]){
+    enableReset = false;
+    buttonGetNumber.classList.add("disabled");
+    setTimeout(() => {
+        if (firstCardSelected[0] === secondCardSelected[0]) {
             foundCards.push(firstCardSelected[0]);
             countUserWin();
             clearCards();
-        }else{
+        } else {
             loserFlag -= 1;
             countUserLifes();
             clearCards();
-            hideCards()
+            hideCards();
         }
-    },1500)
+    }, 1000);
 }
 
-function cardButtonListener (arr,i,element) {
+function cardButtonListener(arr, i, element) {
     // function params: array, index position and HTML element
-    if(foundCards.includes(arr[i])!== true) {
-        if(firstCardSelected[0]===-1&&secondCardSelected[0]===-1){ //true
-            firstCardSelected = [arr[i],i,element]
-            showCardSelected(element,arr[i]) // show the card selected
-
-        }else if(firstCardSelected[0]!==-1&&secondCardSelected[0]===-1){
-            secondCardSelected = [arr[i],i,element]
-            if(firstCardSelected[1]!==secondCardSelected[1]){ //descarta que sea la misma carta
-                showCardSelected(element,arr[i])  // show the card selected
-                //llamar a la funcion que evalua los dos casos y que ponga en -1 a los array 
-                evalCardsValues()
-            }else if(firstCardSelected[1]===secondCardSelected[1]){ //si es la misma carta la borra del array
-                secondCardSelected = [-1,-1,'']
+    if (foundCards.includes(arr[i]) !== true) {
+        if (firstCardSelected[0] === -1 && secondCardSelected[0] === -1) {
+            //true
+            firstCardSelected = [arr[i], i, element];
+            showCardSelected(element, arr[i]); // show the card selected
+        } else if (
+            firstCardSelected[0] !== -1 &&
+            secondCardSelected[0] === -1
+        ) {
+            secondCardSelected = [arr[i], i, element];
+            if (firstCardSelected[1] !== secondCardSelected[1]) {
+                //descarta que sea la misma carta
+                showCardSelected(element, arr[i]); // show the card selected
+                //llamar a la funcion que evalua los dos casos y que ponga en -1 a los array
+                evalCardsValues();
+            } else if (firstCardSelected[1] === secondCardSelected[1]) {
+                //si es la misma carta la borra del array
+                secondCardSelected = [-1, -1, ""];
             }
         }
     }
@@ -114,9 +128,9 @@ function cardButtonListener (arr,i,element) {
 // - 8
 function createCardButons(arr) {
     const cardArrayButtons = document.querySelectorAll("[data-card]");
-    for(let i = 0; i < cardArrayButtons.length; i++){
-        cardArrayButtons[i].addEventListener("click", ()=>{
-            cardButtonListener(arr, i,cardArrayButtons[i]);
+    for (let i = 0; i < cardArrayButtons.length; i++) {
+        cardArrayButtons[i].addEventListener("click", () => {
+            cardButtonListener(arr, i, cardArrayButtons[i]);
         });
     }
 }
@@ -125,7 +139,7 @@ function createCardButons(arr) {
 function createDomElements(arr) {
     cardsContainer.classList.add("cards__container--grid");
     cardsContainer.innerHTML = ``;
-    arr.forEach(el => {
+    arr.forEach((el) => {
         cardsContainer.innerHTML += `
             <div data-card class="card__reverse"></div>`;
     });
@@ -159,40 +173,38 @@ function createRandomArray() {
 
 // - 4
 function resetValues() {
-    setTimeout(()=>{
-        firstCardSelected = [-1,-1];
-        secondCardSelected = [-1,-1];
-        winnerFlag = 0;
-        loserFlag = 10;
-        userCurrentScore=0
-        foundCards = [];
-        currentScore.innerHTML = userCurrentScore
-        userLifes.innerText = `${loserFlag}`;
-        winnerFlagDom.innerText = `${winnerFlag}`;
-    },1500)
+    firstCardSelected = [-1, -1];
+    secondCardSelected = [-1, -1];
+    winnerFlag = 0;
+    loserFlag = 10;
+    userCurrentScore = 0;
+    foundCards = [];
+    currentScore.innerHTML = userCurrentScore;
+    userLifes.innerText = `${loserFlag}`;
+    winnerFlagDom.innerText = `${winnerFlag}`;
 }
 
 // - 3
-function cardsMainFunc(){
-    resetValues();//4
+function cardsMainFunc() {
+    resetValues(); //4
     buttonGetNumber.innerText = `Reset`;
-    let arr = createRandomArray();//5
-    arr=changeToEvenNumber(arr);//6
-    createDomElements(arr);//7
+    let arr = createRandomArray(); //5
+    arr = changeToEvenNumber(arr); //6
+    createDomElements(arr); //7
     createCardButons(arr); //8
 }
 
 // - 2
 function getUser() {
-    userName = userNameInput.value.substring(0,10);
-    if ( userName!==''&& userName.includes(String.fromCharCode(32))!==true){
+    userName = userNameInput.value.trim().substring(0, 10);
+    if (userName !== "") {
         gamePanelName.innerHTML = `<h1>${userName}</h1>`;
-            cardsMainFunc()//3
+        cardsMainFunc(); //3
     } else {
-        errorInput.classList.remove('display__none');
+        errorInput.classList.remove("display__none");
     }
 }
 // - 1
 buttonGetNumber.addEventListener("click", () => {
-    getUser(); //2
+    if (enableReset) getUser(); //2
 });
